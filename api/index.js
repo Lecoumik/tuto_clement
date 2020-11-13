03769c6c2e4ddf79712e697cc0f2f865ec03243d
Itazake <clementparayre@gmail.com>
2020-11-13 23:59:08 +0100

    whaou on lit des données !!!

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const test = require('./test.json')
+const cors = require('cors');
 
+app.use(cors({}));
+
+var pgp = require("pg-promise")(/*options*/);
+var db = pgp("postgres://postgres:beluga@localhost:5432/chat");
+
+
+function bonjour(name) {
+    return "bonjour" + name;
+}
 
 app.listen(8080, () => {
     console.log('bienvenue les minous');
 })
+
 
 app.get('/', (req,res) => {
     res.send("bienvenue dans l'api des minous")
@@ -14,18 +25,30 @@ app.get('/', (req,res) => {
 app.get('/sendMessage/:name/:message', (req,res) => {
     var name = req.params.name;
     var message = req.params.message;
+
+    db.task('post-message-events', async t => {
+        return t.any("INSERT INTO chat (nom, message) VALUES ('"+name+"', '"+message+"')");
+    })
+    .then(data => {
+        res.status(200).json(data);
+    })
+    .catch(error => {
+        res.status(400).json(result);
+    });
     res.send(name+" : "+message);
 })
 
 
-app.get('/historique/:name', (req,res) => {
-    var name = req.params.name;
-    res.send(name+" bientot tous les message ");
-})
-
-
-
-
 app.get('/historique', (req,res) => {
-    res.status(200).json(test);
+    var result = {};
+    // se connecter a la base de donnée
+    db.task('get-historique-events', async t => {
+        return t.any('select * from chat');
+    })
+    .then(data => {
+        res.status(200).json(data);
+    })
+    .catch(error => {
+        res.status(400).json(result);
+    });
 })
--- minou/src/App.js
+++ minou/src/App.js
@@ -1,21 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  
+  const [messages, setMessages] = useState([]);
+
+  function historique() {
+    fetch("http://localhost:8080/historique")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setMessages(result);
+      },
+      (error) => {
+        setMessages("aie");
+      }
+    )
+  }
+
+  useEffect(() => {
+    historique();
+  }, [])
+
   return (
     <div className="App">
         <div className="top-container">
-          <div className="line-summary">
-            <span>Chat</span> <span>:</span> <span>Bonjour</span>
-          </div>
-          <div className="line-summary">
-            <span>Chaton</span> <span>:</span> <span>Salut</span>
-          </div>
+          {messages.map(item => (
+            <div className="line-summary">
+              <span>{item.nom}</span> <span>:</span> <span>{item.message}</span>
+            </div>
+            ))
+          }
         </div>
         <div className="bottom-container">
           <div className="text">
-
           </div>
           <div className="name">
 
@@ -27,5 +45,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
